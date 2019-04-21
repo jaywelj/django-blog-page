059eee05a89176efba079e798d1ef6119a257b5e
jaywelj <jaywelj@gmail.com>
2019-04-21 23:27:27 +0800

    Added extended tutorial features Comments/Authentication

--- blog/views.py
+++ blog/views.py
@@ -6,13 +6,13 @@ from .forms import PostForm, CommentForm
 
 # Create your views here.
 def post_list(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by("-published_date")
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by("published_date")
 	return(render(request, 'blog/post_list.html',{'posts':posts}))
 
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'blog/post_detail.html', {'post':post})
-	
+
 @login_required
 def post_new(request):
 	if request.method == "POST":
@@ -43,7 +43,7 @@ def post_edit(request, pk):
 
 @login_required
 def post_draft_list(request):
-	posts = Post.objects.filter(published_date__isnull=True).order_by("-created_date")
+	posts = Post.objects.filter(published_date__isnull=True).order_by("created_date")
 	return(render(request,"blog/post_draft_list.html", {'posts':posts}))
 
 @login_required
