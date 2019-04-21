6d3f09b7a8eb60f3c0e292b6d61f939a32f3376f
jaywelj <jaywelj@gmail.com>
2019-04-21 23:13:06 +0800

    Added extended tutorial features Comments/Authentication

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,13 @@ from . import views
 
 urlpatterns = [
 	path('', views.post_list, name='post_list'),
-	path('post/<int:pk>/',views.post_detail, name='post_detail'),
-	path('post/new/',views.post_new, name="post_new"),
+	path('post/<int:pk>/', views.post_detail, name='post_detail'),
+	path('post/new/', views.post_new, name="post_new"),
 	path('post/edit/<int:pk>/', views.post_edit, name="post_edit"),
+	path('draft/', views.post_draft_list, name="post_draft_list"),
+	path('post/publish/<int:pk>', views.post_publish, name="post_publish"),
+	path('post/delete/<int:pk>', views.post_delete, name="post_delete"),
+	path('comment/<int:pk>', views.add_comment, name="add_comment"),
+	path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+	path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
 ]
